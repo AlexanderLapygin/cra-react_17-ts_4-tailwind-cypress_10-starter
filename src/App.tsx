3857a13bb3a668d8b28b509a05737d2cb0301f74
softspider <sftspider@gmail.com>
2022-08-07 19:28:00 +0300

    test: update App

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
     return (
-        <h1 className="container mx-auto text-center py-2 text-white font-bold italic text-5xl bg-red-500">
+        <h1 className="container rounded-full hover:bg-red-700 mx-auto text-center py-2 text-white font-bold italic text-5xl bg-red-500">
             Hello!
         </h1>
     );
